9398da829f3875e77a0031d2db04ddd14a8f8a3d
Ben Taljaard <ben.taljaard@devoteam.com>
2019-02-26 10:38:46 +0100

    add preparation script for installation host

--- scripts/prepare_installing_host.sh
+++ scripts/prepare_installing_host.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+# This script needs to be run as root
+# Setup installation host (this can be the first node or a bastion host)
+
+# Configure proxy
+export http_proxy=http://172.23.29.156:3128
+export https_proxy=http://172.23.29.156:3128
+
+
+# Enable required repositories
+subscription-manager repos --enable rhel-7-server-ose-3.11-rpms
+subscription-manager repos --enable rhel-7-server-ansible-2.6-rpms
+
+
+# Install openshift ansible + playbooks
+yum install openshift-ansible -y
+yum install atomic-openshift-clients -y
+
