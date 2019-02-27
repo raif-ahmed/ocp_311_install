a2c7fe964f6f194beccf59eeedd2d34c8327b2c9
Ben Taljaard <ben.taljaard@devoteam.com>
2019-02-27 12:42:54 +0100

    comment out hostname change

--- scripts/fix_host_config_np.sh
+++ scripts/fix_host_config_np.sh
@@ -3,30 +3,33 @@
 # Script to fix host configuration :-(
 
 
-# Add network routes (might need to do this manually)
-
-
 # Add proxy config
+echo "Adding proxy configuration"
 echo "export https_proxy=http://172.23.29.156:3128" > /etc/profile.d/proxy.sh
 echo "export http_proxy=http://172.23.29.156:3128" >> /etc/profile.d/proxy.sh
 chmod +x /etc/profile.d/proxy.sh
+echo "[DONE]"
 
 # Enable RHN repos
+echo "Enable RHN repos"
 subscription-manager repos --enable rhel-7-server-ose-3.11-rpms
 subscription-manager repos --enable rhel-7-server-ansible-2.6-rpms
+echo "[DONE]"
 
 # Set FQ hostname
-HOSTNAME_SUFFIX=-svc.cpn.lgi.inf.libgbl.biz
-CURRENT_HOSTNAME=$(hostname)
+# echo "Set hostname"
+# HOSTNAME_SUFFIX=-svc.cpn.lgi.inf.libgbl.biz
+# CURRENT_HOSTNAME=$(hostname)
 
-if [[ $CURRENT_HOSTNAME == *"$HOSTNAME_SUFFIX"* ]]; then
-  	echo "Hostname set correctly"
-else
-	hostnamectl set-hostname $CURRENT_HOSTNAME$HOSTNAME_SUFFIX
-fi
-
+# if [[ $CURRENT_HOSTNAME == *"$HOSTNAME_SUFFIX"* ]]; then
+#   	echo "Hostname set correctly"
+# else
+# 	hostnamectl set-hostname $CURRENT_HOSTNAME$HOSTNAME_SUFFIX
+# fi
+# echo "[DONE]"
 
 # Fix log and var disk sizes
+echo "Fix log and var disk sizes"
 lvresize --size 10G /dev/vgroot/lv_log -r
 lvresize --size 8G /dev/vgroot/lv_var -r
 
