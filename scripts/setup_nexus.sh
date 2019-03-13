0818880b2ecd96f37bafe9a648f9aebcf1695539
Ben Taljaard <ben.taljaard@devoteam.com>
2019-03-13 21:11:57 +0100

    add nexus scripts

--- scripts/setup_nexus.sh
+++ scripts/setup_nexus.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
-# Setup Nexus Project
-if [ "$#" -ne 1 ]; then
-    echo "Usage:"
-    echo "  $0 GUID"
-    exit 1
+# Create CICD project
+if (( $(oc get projects|grep cicd|wc -l) == 0 )); then
+  oc new-project cicd
+  oc label namespace cicd "name=cicd"
 fi
 
-GUID=$1
-echo "Setting up Nexus in project $GUID-nexus"
 
 
-
-# Ensure that we are creating the objects in the correct project
-oc project ${GUID}-nexus
-
-# Call template to provision nexus objects
-oc new-app -f Infrastructure/templates/nexus3.yaml -p GUID=${GUID} -p CPU_LIMITS=1000m -p MEM_REQUESTS=1Gi -p MEM_LIMITS=2Gi -p VOLUME_CAPACITY=2G -n ${GUID}-nexus
+if (( $(oc get dc -n cicd|grep nexus|wc -l) == 0 )); then
+	# Call template to provision nexus objects
+	oc new-app -f /home_ldap/btaljaard/ocp_311_install/resources/templates/nexus3.yaml \
+		-p GUID=${GUID} -p CPU_LIMITS=1000m -p MEM_REQUESTS=1Gi \
+		-p MEM_LIMITS=2Gi -p VOLUME_CAPACITY=2G -n cicd
+fi
 
 # Wait for nexus to start before we configure it
 while : ; do
   echo "Checking if Nexus is Ready..."
-  oc get pod -n ${GUID}-nexus|grep -v deploy|grep "1/1"
+  oc get pod -n cicd|grep -v deploy|grep "1/1"
   [[ "$?" == "1" ]] || break
   echo "...no. Sleeping 10 seconds."
   sleep 10
@@ -30,9 +27,9 @@ done
 sleep 5
 
 # Run configuration script to configure redhat maven repos, create release repo, configure proxy for maven, setup docker registry repo
-curl -o config_nexus_tmp.sh -s https://raw.githubusercontent.com/bentaljaard/rh_appdev_homework/master/Infrastructure/bin/config_nexus3.sh
+curl -o config_nexus_tmp.sh -s https://raw.githubusercontent.com/bentaljaard/ocp_311_install/master/scripts/config_nexus3.sh
 chmod +x config_nexus_tmp.sh
-./config_nexus_tmp.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}' -n ${GUID}-nexus)
+./config_nexus_tmp.sh admin admin123 https://$(oc get route nexus3 --template='{{ .spec.host }}' -n cicd)
 rm config_nexus_tmp.sh
 
 echo "************************"
